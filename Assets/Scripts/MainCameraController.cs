b097539db770852eb046d311eec79f37c8c98de6
Petras Zdanavičius <petraszd@gmail.com>
2019-01-26 20:26:41 +0200

    Main Camera

---
+++ Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MainCameraController : MonoBehaviour
+{
+    public CinemachineVirtualCamera m_virtualCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (m_virtualCamera.Follow == null) {
+            m_virtualCamera.Follow = GameObject.Find("Girl").GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
