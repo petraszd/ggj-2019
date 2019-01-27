651030dd1471b5a30abd66d0915c59482a608c1e
Petras Zdanavičius <petraszd@gmail.com>
2019-01-27 14:21:26 +0200

    Start screen

---
+++ Assets/Scripts/QuitListener.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitListener : MonoBehaviour
+{
+  public static QuitListener instance = null;
+
+  private void Awake()
+  {
+    if (instance == null) {
+      instance = this;
+    } else if (instance != this) {
+      Destroy(gameObject);
+    }
+    DontDestroyOnLoad(gameObject);
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape) | Input.GetKeyDown(KeyCode.Q)) {
+      Application.Quit();
+    }
+  }
+}
+
---
+++ Assets/Scripts/RunMainManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunMainManager : MonoBehaviour
+{
+  public float Timout = 0.1f;
+
+  bool isOn = false;
+
+  void Start()
+  {
+    isOn = false;
+    StartCoroutine(TurnOnUIDelayed());
+  }
+
+  void Update()
+  {
+    if (!isOn) {
+      return;
+    }
+
+    if (Input.touches.Length > 0 || Input.GetMouseButtonUp(0) || Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return)) {
+      UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
+    }
+  }
+
+  IEnumerator TurnOnUIDelayed()
+  {
+    yield return new WaitForSeconds(Timout);
+    isOn = true;
+  }
+}
+
