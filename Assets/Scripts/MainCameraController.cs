deb9d37495ac3352286a1c70dd60095a1dba9711
MatasRR <matasr10@gmail.com>
2019-01-27 12:02:52 +0200

    Camera controller

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -5,20 +5,61 @@ using Cinemachine;
 
 public class MainCameraController : MonoBehaviour
 {
-    public CinemachineVirtualCamera m_virtualCamera;
+    //public CinemachineVirtualCamera m_virtualCamera;
+    public Rect CameraLimits;
+    public Texture _Texture;
+    private Transform Girl;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        /*
         if (m_virtualCamera.Follow == null) {
             m_virtualCamera.Follow = GameObject.Find("Girl").GetComponent<Transform>();
-        }
+        }*/
+        Girl = GameObject.Find("Girl").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float X, Y;
 
+        if (Girl.position.x > CameraLimits.xMax)
+        {
+            X = CameraLimits.xMax;
+        }
+        else if (Girl.position.x < CameraLimits.xMin)
+        {
+            X = CameraLimits.xMin;
+        }
+        else
+        {
+            X = Girl.transform.position.x;
+        }
+        if (Girl.position.y > CameraLimits.yMax)
+        {
+            Y = CameraLimits.yMax;
+        }
+        else if (Girl.position.y < CameraLimits.yMin)
+        {
+            Y = CameraLimits.yMin;
+        }
+        else
+        {
+            Y = Girl.transform.position.y;
+        }
+        /*
+        X = Mathf.Max(CameraLimits.xMin, Girl.position.x);
+        X = Mathf.Min(CameraLimits.xMax, Girl.position.x);
+        Y = Mathf.Max(CameraLimits.yMin, Girl.position.y);
+        Y = Mathf.Min(CameraLimits.yMax, Girl.position.y);
+        */
+        transform.position = new Vector3(X, Y, 0);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawGUITexture(CameraLimits, _Texture);
     }
 }
